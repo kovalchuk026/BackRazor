84ec33e5ffc2dd6f9822dfdd670b61fbbfbbe1e2
kovalchuk026 <kovalchuk026@gmail.com>
2023-12-12 22:54:09 +0600

    modified:   ViT/models/modeling.py 	modified:   ViT/models/modeling_new_prune.py 	new file:   custom_functions/custom_fc.py 	new file:   custom_functions/custom_gelu.py 	new file:   custom_functions/custom_matmul.py 	new file:   custom_functions/custom_softmax.py 	new file:   custom_functions/custom_softmax_matmul.py

--- ViT/models/modeling.py
+++ ViT/models/modeling.py
@@ -162,10 +162,6 @@ class Embeddings(nn.Module):
             n_patches = (img_size[0] // patch_size[0]) * (img_size[1] // patch_size[1])
             self.hybrid = False
 
-        if self.hybrid:
-            self.hybrid_model = ResNetV2(block_units=config.resnet.num_layers,
-                                         width_factor=config.resnet.width_factor)
-            in_channels = self.hybrid_model.width * 16
         self.patch_embeddings = Conv2d(in_channels=in_channels,
                                        out_channels=config.hidden_size,
                                        kernel_size=patch_size,
