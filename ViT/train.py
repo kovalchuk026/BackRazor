af2f9fdc66107a799dec5fc4a1587bcdd0167162
kovalchuk026 <kovalchuk026@gmail.com>
2023-12-12 17:10:16 +0600

    update

--- ViT/train.py
+++ ViT/train.py
@@ -1,1 +1,23 @@
-import pytorch as pt
+# coding=utf-8
+from __future__ import absolute_import, division, print_function
+
+import sys
+sys.path.append(".")
+import argparse
+
+from datetime import timedelta
+
+import torch
+import torch.distributed as dist
+
+from torch.utils.tensorboard import SummaryWriter
+
+from ViT.utils.scheduler import WarmupLinearSchedule, WarmupCosineSchedule
+from ViT.utils.data_utils import get_loader
+from ViT.utils.utils import *
+from ViT.utils.memory_cost_profiler import profile_memory_cost
+
+import time
+import mesa as ms
+
+from torch import nn
