784ba62471ab08d47af27b694d215b66e512e70b
kovalchuk026 <kovalchuk026@gmail.com>
2023-12-13 11:11:18 +0600

    cifar10.sh

--- cifar10.sh
+++ cifar10.sh
@@ -12,4 +12,4 @@ ViT/train.py --name cifar10-lr${lr}-B128-BackRazor${backPruneRatio} --learning_r
 --dataset cifar10 --model_type ViT-Ti_16 --pretrained_dir ${save_dir}/pretrain/ViT-Ti_16.npz \
 --new_backrazor --back_prune_ratio ${backPruneRatio} \
 --train_batch_size 128 --eval_batch_size 32 \
---num_steps 3000 --eval_every 250
+--num_steps 3000 --eval_every 500
